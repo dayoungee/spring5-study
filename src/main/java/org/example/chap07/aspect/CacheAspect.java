edcda96e2daf7ca736fa75aec65f9de59e1e03e9
Dayoung <dbsek21@naver.com>
2023-02-22 22:33:12 +0900

    Feat: modify print

--- src/main/java/org/example/chap07/aspect/CacheAspect.java
+++ src/main/java/org/example/chap07/aspect/CacheAspect.java
@@ -20,12 +20,12 @@ public class CacheAspect {
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         Long num = (Long) joinPoint.getArgs()[0];
         if (cache.containsKey(num)) {
-            System.out.printf("CacheAspect: Cache 에 서 구함 [%d] \n", num);
+            System.out.printf("CacheAspect: Cache 에서 구함 [%d] \n", num);
             return cache.get(num);
         }
         Object result = joinPoint.proceed();
         cache.put(num, result);
-        System.out.printf("CacheAspect: Cache 에 가 [%d] \n",num);
+        System.out.printf("CacheAspect: Cache에 추가 [%d] \n",num);
         return result;
     }
 }
